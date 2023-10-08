2e450fb1a3ae947f4929dfb906982948b7e2adeb
Dristi <103329082+Dris-ti@users.noreply.github.com>
2023-10-09 02:51:24 +0600

    Add files via upload

---
+++ 1807A_Plus_or_Minus.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <array>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    vector<string> v;
+
+    while (t--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        int mx = max(a, b);
+        cout << mx << " " << c << endl;
+        if (c > mx)
+        {
+            v.push_back("+");
+        }
+        else
+        {
+            v.push_back("-");
+        }
+    }
+
+    for (int j = 0; j < v.size(); j++)
+    {
+        cout << v[j] << endl;
+    }
+}
